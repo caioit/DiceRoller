16ac4ad8e139d6b41fe4ea42caf82ff585508d5e
caioit <caioit@users.noreply.github.com>
2020-08-23 16:35:06 -0300

    Atualização

--- dice_roller.py
+++ dice_roller.py
@@ -1,17 +1,29 @@
 import PySimpleGUI as sg
 import random
 
-sides = [1, 2, 3, 4, 5, 6]
+class Dice:
+    def __init__(self, number_of_sides):
+        self.number_of_sides = number_of_sides
+        self.side = []
+        
+        for i in range(number_of_sides):
+            self.side.append(i + 1)
+    
+    def print_sides(self):
+        print(self.side)
+
+number_of_sides = 6
+dice = Dice(number_of_sides)
 
 #Generate a random number 
 def randomization(sides):
     return(random.choice(sides))
 
-result = randomization(sides)
+result = randomization(dice.side)
 
 #Elements in the Window
 layout = [
-    [sg.Text(result, key='RESULT')],
+    [sg.Text(result, key='RESULT', size=(30,2))],
     [sg.Button('Roll'),sg.Button('Exit')]
 ]
 
@@ -25,7 +37,7 @@ while True:
         break
 
     if event == 'Roll':
-        result = randomization(sides)
+        result = randomization(dice.side)
         window['RESULT'].update(result)
 
     print(result)
